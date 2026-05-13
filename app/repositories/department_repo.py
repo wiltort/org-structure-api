a1d15f99d72c43e610ce15598b4f4ee73c1cb8a2
Wiltort <wiltort21@gmail.com>
2026-05-13 20:17:17 +0500

    feat: complete departaments repo

--- app/repositories/department_repo.py
+++ app/repositories/department_repo.py
@@ -1,6 +1,6 @@
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy import func, select
-from sqlalchemy.orm import Session
+from sqlalchemy.orm import Session, aliased
 from app.models.departments import Department, Employee
 from datetime import datetime
 from app.core.config import settings
@@ -13,104 +13,71 @@ class DepartamentRepository:
     def get_by_id(
         db: Session, department_id: int
     ) -> Department | None:
-        result = db.execute(
-            select(Department).where(Department.id == department_id)
-        )
-        return result.scalar_one_or_none()
-
+        return db.query(Department).get(department_id)
+ 
     @staticmethod
-    def create(
-        db: Session,
-        name: str,
-        parent_id: int | None = None,
-    ) -> Department:
+    def get_children(db: Session, parent_id: int) -> list[Department]:
+        return db.query(Department).filter(Department.parent_id == parent_id).all()
+    
+    @staticmethod
+    def get_employees(db: Session, department_id: int) -> list[Employee]:
+        return db.query(Employee).filter(Employee.department_id == department_id).all()
+    
+    @staticmethod
+    def create(db: Session, name: str, parent_id: int | None = None) -> Department:
         department = Department(name=name, parent_id=parent_id)
         db.add(department)
         db.commit()
+        db.refresh(department)
         return department
-    
-    @staticmethod
+
+# перенести в сервис
+    @classmethod
+    def get_children_tree(
+        cls,
+        db: Session,
+        parent_id: int,
+        depth: int = 1
+    ) -> list[dict[str, any]]:
+        children = db.query(Department).filter(Department.parent_id == parent_id).all()
+        result = []
+        if depth <= 1:
+            for child in children:
+                result.append({
+                    'id': child.id,
+                    'name': child.name,
+                    'created_at': child.created_at,    
+                })
+            return result
+        for child in children:
+            result.append({
+                'id': child.id,
+                'name': child.name,
+                'created_at': child.created_at,
+                'children': cls.get_children_tree(db, child.id, depth-1),
+            })
+        return result
+
+    @classmethod
     def get_tree_and_employees(
+        cls,
         db: Session,
         department_id: int,
         depth: int = 1,
-    ) -> list[Department]:
-        return db.execute(
-            select(Department).where(Department.parent_id == department_id)
-        )
+        include_employees: bool = False,
+    ) -> dict[str, any] | None:
+        
+        department = db.query(Department).get(department_id)
 
-    @staticmethod
-    async def get_last_price_record(
-        db: AsyncSession, ticker: str
-    ) -> models.PriceRecord | None:
-        stmt = select(models.PriceRecord)
-        if ticker:
-            stmt = stmt.where(models.PriceRecord.ticker == ticker)
-        stmt = stmt.order_by(models.PriceRecord.timestamp.desc()).limit(1)
-        return (await db.execute(stmt)).scalar_one_or_none()
+        if not department:
+            return None
+        result = {
+            'department': department,
+            'employees': [],
+            'children': cls.get_children_tree(db, department_id, depth),
+        }
+        if include_employees:
+            result['employees'] = db.query(Employee).filter(Employee.department_id == department_id).all()
+        return result
 
-    @staticmethod
-    async def create_price_record(
-        db: AsyncSession, price_record_data: dict
-    ) -> models.PriceRecord:
-        db_price_record = models.PriceRecord(**price_record_data)
-        db.add(db_price_record)
-        await db.commit()
-        await db.refresh(db_price_record)
-        return db_price_record
-
-    @staticmethod
-    async def get_price_records(
-        db: AsyncSession,
-        ticker: str | None = None,
-        start_date: datetime | None = None,
-        end_date: datetime | None = None,
-        desc: bool = False,
-        pagination: bool = True,
-        page: int = 1,
-        page_size: int = settings.page_size,
-    ) -> list[models.PriceRecord]:
-        """Получение записей цен"""
-        stmt = select(models.PriceRecord)
-
-        if ticker:
-            stmt = stmt.where(models.PriceRecord.ticker == ticker)
-
-        if start_date:
-            stmt = stmt.where(models.PriceRecord.timestamp >= start_date)
-
-        if end_date:
-            stmt = stmt.where(models.PriceRecord.timestamp <= end_date)
-        if desc:
-            stmt = stmt.order_by(models.PriceRecord.timestamp.desc())
-        else:
-            stmt = stmt.order_by(models.PriceRecord.timestamp)
-
-        # Пагинация
-        if pagination:
-            offset = (page - 1) * page_size
-            stmt = stmt.offset(offset).limit(page_size)
-
-        return (await db.execute(stmt)).scalars().all()
-
-    @staticmethod
-    async def get_total_and_pages(
-        db: AsyncSession,
-        ticker: str | None = None,
-        start_date: datetime | None = None,
-        end_date: datetime | None = None,
-        page_size: int = settings.page_size,
-        **kwargs,
-    ) -> dict[str, int]:
-        """Получение общего количества записей и общего количества страниц"""
-        stmt = select(func.count(models.PriceRecord.id))
-        if ticker:
-            stmt = stmt.where(models.PriceRecord.ticker == ticker)
-        if start_date:
-            stmt = stmt.where(models.PriceRecord.timestamp >= start_date)
-        if end_date:
-            stmt = stmt.where(models.PriceRecord.timestamp <= end_date)
-        result = await db.execute(stmt)
-        total = result.scalar() or 0
-        pages = total // page_size + (1 if total % page_size > 0 else 0)
-        return {"total": total, "pages": pages}
+                
